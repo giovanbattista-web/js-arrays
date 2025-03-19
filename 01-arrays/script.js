f4c7ad2b286b3f2d322f01bdecfc9133950a9d83
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-19 17:13:20 +0100

    [UPD] TROVARE L'INDICE E SALVARLO IN UNA VARIABILE

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -8,18 +8,22 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+// console.log(teachers[3]);
+
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher =['Phil'];
+const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4,1,'Patrick');
+teachers[4] = "Mario";
+teachers.splice(4, 1, 'Patrick');
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
 console.log(teachers);
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
@@ -35,8 +39,20 @@ console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
 
+for (let i = 0; i <= 6; i++) {
+  if (teachers[i] == "Lewis") {
+    console.log("trovato", i);
+    const lewisIndex = i;
+  }
+
+}
+/* BONUS : 
+1 CREA UN CICLO CHE STAMPA I NUMERI DA 0 A 6 
+2 CREA UN CICLO CHE STAMPA I TEACHERS IN POSIZIONE DA 0 A 6 
+3 CREA UN CICLO CHE STAMPA ENTRAMBI. ESEMPIO 0 MARIO 1 PEPPE ECC...
+*/
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = null;
+
