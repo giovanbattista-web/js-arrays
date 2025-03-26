ae6644277504b13cfc6fd7c5cad6ccab5106faf2
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-26 09:22:35 +0100

    [UPD] CORREZIONE DEL PROFESSORE

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,18 +14,17 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[4] = "Mario";
-teachers.splice(4, 1, 'Patrick');
+teachers[4] = "Patrick";
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(teachers);
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
 console.log(teachers);
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
@@ -37,35 +36,35 @@ console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-
-for (let i = 0; i <= 6; i++) {
-  if (teachers[i] == "Lewis") {
-    console.log("trovato", i);
-    const lewisIndex = i;
+let index;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Lewis") {
+    index = i;
   }
-
 }
+const lewisIndex = index;
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-for (let i = 0; i <= 6; i++) {
-  if (teachers.length == 0) {
-    console.log("array vuoto");
-  }
-  else {
-    console.log("nell'array sono presenti degli elementi");
-  }
+const isTeachersEmpty =  teachers.length === 0;
+if (isTeachersEmpty){
+  console.log(`L'array è vuoto: ${isTeachersEmpty}`);
 }
-const isTeachersEmpty = "nell'array sono presenti degli elementi";
+else 
+{
+  console.log(`L'array non è vuoto: ${isTeachersEmpty}`);
+}
+  
 
 
 // 1 CREA UN CICLO CHE STAMPA I NUMERI DA 0 A 6 
 // 2 CREA UN CICLO CHE STAMPA I TEACHERS IN POSIZIONE DA 0 A 6 
 // 3 CREA UN CICLO CHE STAMPA ENTRAMBI. ESEMPIO 0 MARIO 1 PEPPE ECC...
-
+/*
 for (let i = 0; i <= 6; i++) {
   console.log(i);
-} 
+}
 
 for (let i = 0; i <= 6; i++) {
   console.log(teachers[i]);
@@ -73,6 +72,7 @@ for (let i = 0; i <= 6; i++) {
 
 for (let i = 0; i <= 6; i++) {
   console.log(teachers);
-} 
+}
+*/
 
 
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,15 +10,22 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-for (let i=6;i>=0;i--) {
-  console.log(teachers[i]);
-  const reversedTeachers = teachers[i];
-} 
-
+let temp =[];
+for (let i=teachers.length-1;i>=0;i--) {
+  temp.push(teachers[i]);
+}
+const reversedTeachers = temp;
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i=0;i<teachers.length;i++){
+  if(teachers[i].length>=5){
+    longNames.push(teachers[i]);
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(1,1);
@@ -26,15 +33,24 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-for (let i = 0; i <= 6; i++) {
-  if (teachers[i] != 0) {
-    console.log("Fabio è presente nell'array");
-  }
-  else {
-    console.log("Fabio non è presente nell'array");
+let present = false;
+for(let i=0;i<teachers.length;i++){
+  if (teachers[i] === "Fabio"){
+    present = true;
   }
 }
-const isFabioPresent = "Fabio è presente nell'array";
+const isFabioPresent = present;
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let tempString = '';
+for (let i=0;i<teachers.length;i++){
+  if(i!==teachers.length-1){
+    tempString+=teachers[i]+", ";
+  } 
+  else {
+    tempString+=teachers[i];
+  }
+}
+const teachersString = tempString;
+console.log(teachersString);
